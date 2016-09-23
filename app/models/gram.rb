1ce86631f06440ae158b23a51ab59af7c5757dfa
Creeland Provinsal <Cree@Creelands-MacBook-Air.local>
2016-09-22 22:02:47 -0700

    Styled the website

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,8 +1,8 @@
 class Gram < ActiveRecord::Base
+  has_many :comments
+  belongs_to :user
   mount_uploader :picture, PictureUploader
+  
   validates :message, presence: true
   validates :picture, presence: true
-
-
-  belongs_to :user
 end
